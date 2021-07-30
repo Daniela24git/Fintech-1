3bc4f9fba331fbe99c77339b0df9442f51c1bc3f
Robin-Rivadeneira <rivadeneirarobin@gmail.com>
2021-07-30 10:17:00 -0500

    cliente Arreglos

---
+++ Cliente/src/modelos/categoria.js
@@ -0,0 +1,22 @@
+const categorias = (sequelize, type)=>{
+    return sequelize.define('categorias', {
+        id:{
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        categoria: type.STRING,
+        createdAt:{
+            type: 'TIMESTAMP',
+            defaultValue: type.literal('CURRENT_TIMESTAMP'),
+            allowNull: false
+        },
+        updatedAt:{
+            type: 'TIMESTAMP',
+            defaultValue: type.literal('CURRENT_TIMESTAMP '),
+            allowNull: false
+        }
+    })
+}
+
+module.exports = categorias
