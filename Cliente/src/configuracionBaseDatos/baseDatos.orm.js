bc3ec22832963b66d3dea5251c7e742414f139d0
Robin-Rivadeneira <rivadeneirarobin@gmail.com>
2021-07-30 10:19:58 -0500

    cliente Arreglos

--- Cliente/src/configuracionBaseDatos/baseDatos.orm.js
+++ Cliente/src/configuracionBaseDatos/baseDatos.orm.js
@@ -1,30 +1,30 @@
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(
-    'fintech', 
-    'root', 
-    '', 
-    {
+  'fintech',
+  'root',
+  '',
+  {
     host: 'localhost',
     dialect: 'mysql',
-    pool:{
-      max:5,
-      min:0,
-      require:30000,
+    pool: {
+      max: 5,
+      min: 0,
+      require: 30000,
       idle: 10000
-     }
     }
-  )
-  
-  sequelize.authenticate()
-    .then(() => {
-      console.log('Conectado')
-    })
-    .catch(err => {
-      console.log('No se conecto')
-    })
-  
-    sequelize.sync({ force: false})
-    .then(() =>{
-      console.log("Tablas sincronizadas")
-    })
+  }
+)
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conectado')
+  })
+  .catch(err => {
+    console.log('No se conecto')
+  })
+
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log("Tablas sincronizadas")
+  })
