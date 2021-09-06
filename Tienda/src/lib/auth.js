93ebbb3b7cdbcb3a57461bd40323638f64e2d811
Robin-Rivadeneira <rivadeneirarobin@gmail.com>
2021-09-06 10:41:30 -0500

    responsibe - arreglos

--- Tienda/src/lib/auth.js
+++ Tienda/src/lib/auth.js
@@ -2,6 +2,7 @@ module.exports = {
     isLoggedIn(req, res, next) {
         if (req.isAuthenticated()) {
             return next();
+        res.redirect('/tienda');
         }else{
             res.redirect('/Login');
         }
